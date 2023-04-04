454c11083407abaae369800ef25f2c938b45bd69
Lê Ngọc Thảo <lengocthao.dev@gmail.com>
2023-04-04 17:05:26 +0700

    update_output

--- Branch&Bound/main.py
+++ Branch&Bound/main.py
@@ -1,6 +1,7 @@
 import heapq
 import time
 import psutil
+
 def Branch_And_Bound(W, m, weights, values, classes):
     n = len(weights)
     root = (0, set(), 0, 0, [])
@@ -30,7 +31,7 @@ def Branch_And_Bound(W, m, weights, values, classes):
 start_time = time.time()
 proccess = psutil.Process()  
 # Read input from file
-with open('./TEST_CASE/INPUT_2.txt', 'r') as f:
+with open('./TEST_CASE/INPUT_10.txt', 'r') as f:
     W = float(f.readline())   # Knapsack capacity
     m = int(f.readline())     # Number of classes
     weights = list(map(float, f.readline().strip().split(',')))   
@@ -41,10 +42,10 @@ with open('./TEST_CASE/INPUT_2.txt', 'r') as f:
 best_value, best_selected = Branch_And_Bound(W, m, weights, values, classes)
 
 # Write output to file
-with open('./RESULT/OUTPUT_2.txt', 'w') as f:
+with open('./RESULT/OUTPUT_10.txt', 'w') as f:
     f.write(str(best_value) + '\n')
     f.write(' '.join(map(str, best_selected)) + '\n')
     end_time = time.time()
     f.close()
-print("Time run: "+ str((end_time-start_time)*1000)+ "ms")
+print("Time run: "+ str((end_time-start_time))+ "s")
 print("Memory used: "+ str(proccess.memory_info().rss/1024/1024)+" MB")
